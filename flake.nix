88837438d63735502a7f54817fc9a2e0af040d63
Lenna Mothburn <activator112233@gmail.com>
2026-04-18 20:07:39 +0200

    grr

--- flake.nix
+++ flake.nix
@@ -1,18 +1,20 @@
 {
   description = "momosoft plymouth theme";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.11";
 
   outputs = { self, nixpkgs }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
     packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
       name = "plymouth-momosoft";
-      src = "./theme";
+      src = ./theme;
 
+      dontUnpack = true;
+      
       installPhase = ''
         mkdir -p $out/share/plymouth/themes/momosoft
-        cp -r . $out/share/plymouth/themes/momosoft
+        cp -r $src/. $out/share/plymouth/themes/momosoft
       '';
     };
-  }
+  };
 }
