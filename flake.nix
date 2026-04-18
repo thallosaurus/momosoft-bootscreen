4dd645dd8846ba6f26ce8cd3e85464078fa35776
Lenna Mothburn <activator112233@gmail.com>
2026-04-18 19:21:28 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "momosoft plymouth theme";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+      name = "plymouth-momosoft";
+      src = "./theme";
+
+      installPhase = ''
+        mkdir -p $out/share/plymouth/themes/momosoft
+        cp -r . $out/share/plymouth/themes/momosoft
+      '';
+    };
+  }
+}
